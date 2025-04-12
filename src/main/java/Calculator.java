4f2afa951d68cfb1440e58e99e4eada887f6f511
matthew <g00430349@atu.ie>
2025-04-12 18:41:07 +0100

    added exception for +, -, /

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,12 +3,23 @@ public int add (int a, int b) {
     return a + b;
 }
 public int subtract (int a, int b) {
-    return a - b;
+    int result = a - b;
+    if (result < 0) {
+        throw new ArithmeticException("Cannot subtract from zero");
+    }
+    return result;
 }
 public int multiply (int a, int b) {
-    return a * b;
+    long result = a * b;
+    if (result < 0) {
+        throw new ArithmeticException("Cannot multiply from zero");
+    }
+    return (int) result;
 }
 public int divide (int a, int b) {
+    if (b == 0) {
+        throw new ArithmeticException("Cannot divide by zero");
+    }
     return a / b;
 }
 
