88e946e4e090b12ce27b045bc6e7770f8b1febf9
matthew <g00430349@atu.ie>
2025-04-12 18:52:25 +0100

    added tests for exceptions

--- src/main/java/calcTEST.java
+++ src/main/java/calcTEST.java
@@ -13,11 +13,23 @@ public class calcTEST {
         assertEquals(2,calc.subtract(2,4));
     }
     @Test
+    void testSubtractionNegativeThrowsException() {
+        assertThrows(IllegalStateException.class, () -> calculator.subtract(3, 5));
+    }
+    @Test
     void testmul(){
         assertEquals(20,calc.multiply(2,10));
     }
     @Test
+    void testMultiplicationOverflowThrowsException() {
+        assertThrows(UnsupportedOperationException.class, () -> calculator.multiply(Integer.MAX_VALUE, 2));
+    }
+    @Test
     void testdiv(){
         assertEquals(5,calc.divide(10,2));
+    }
+    @Test
+    void testDivisionByZeroThrowsException() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
     }
 }
