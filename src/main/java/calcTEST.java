e3d18b78c1d80be2270c757fdb444a1622fbca6f
matthew <g00430349@atu.ie>
2025-04-12 18:49:07 +0100

    added tests for each multiple

--- src/main/java/calcTEST.java
+++ src/main/java/calcTEST.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class calcTEST {
+    Calculator calc = new Calculator();
+
+    @Test
+    void testadd(){
+        assertEquals(10,calc.add(7,2));
+    }
+    @Test
+    void testsub(){
+        assertEquals(2,calc.subtract(2,4));
+    }
+    @Test
+    void testmul(){
+        assertEquals(20,calc.multiply(2,10));
+    }
+    @Test
+    void testdiv(){
+        assertEquals(5,calc.divide(10,2));
+    }
 }
